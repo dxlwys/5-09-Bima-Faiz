7b18b8ffa6965ee64b3820ca864e5e42baff7d88
dxlwys <3103120051@student.smktelkom-pwt.sch.id>
2022-01-26 08:36:14 +0700

    third commit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,13 +7,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link {{ ( $title == 'Home') ? 'active' : ''}}" aria-current="page" href="/">Home</a>
+                    <a class="nav-link {{ ( $title == 'Home') ? 'active' : ''}}" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link {{ ( $title == 'About') ? 'active' : ''}}" href="/about">About</a>
+                    <a class="nav-link {{ ( $title == 'About') ? 'active' : ''}}" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link {{ ( $title == 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
+                    <a class="nav-link {{ ( $title == 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ ( $title == 'Contacts') ? 'active' : ''}}" href="/contacts">Contacts Us</a>
                 </li>
                 
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,8 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+Route::resource('/contacts', ContactController::class);
+    return view('contacts', [
+        "title" => "Contacts"
+    ]);
